02dea8bbca536fb6a363f45ae419cd46911e62e6
Dmitri Gorun <ss808@mail.ru>
2023-02-26 13:36:39 +0500

    change api ori

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,5 @@
-export const API_URI = 'http://localhost:3024';
+// export const API_URI = 'http://localhost:3024';
+export const API_URI = 'https://mini-nice-ringer.glitch.me';
 export const POSTFIX = '/api/product';
 
 
